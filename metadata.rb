70596ed06ba89cbfff0f333149f8c22f75dfb959
Aaron Kalin <akalin@martinisoftware.com>
2013-03-11 02:48:08 -0500

    Some quoting consistency

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "funnies"
-maintainer       "Aaron Kalin"
-maintainer_email "akalin@martinisoftware.com"
-license          "Apache 2.0"
-description      "Installs/Configures/Deploys funnies"
+name             'funnies'
+maintainer       'Aaron Kalin'
+maintainer_email 'akalin@martinisoftware.com'
+license          'Apache 2.0'
+description      'Installs/Configures/Deploys funnies'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
 
 depends 'application', '2.0.0'
 depends 'build-essential'

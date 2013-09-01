aff6cc734f82d11bbad2e271c0037436391ae66b
Aaron Kalin <akalin@martinisoftware.com>
2013-09-01 03:51:35 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'akalin@martinisoftware.com'
 license          'Apache 2.0'
 description      'Installs/Configures/Deploys funnies'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.2'
 
 depends 'application', '2.0.0'
 depends 'build-essential'

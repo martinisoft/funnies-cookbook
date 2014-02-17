9947b88b2b42542414dc021fdc46cdd1c8d2d195
Aaron Kalin <akalin@martinisoftware.com>
2014-02-17 14:21:46 -0600

    Swap quotes

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,17 @@
-name             'funnies'
-maintainer       'Aaron Kalin'
-maintainer_email 'akalin@martinisoftware.com'
-license          'Apache 2.0'
-description      'Installs/Configures/Deploys funnies'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.8'
+name             "funnies"
+maintainer       "Aaron Kalin"
+maintainer_email "akalin@martinisoftware.com"
+license          "Apache 2.0"
+description      "Installs/Configures/Deploys funnies"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "1.3.8"
 
-depends 'application', '2.0.0'
-depends 'build-essential'
-depends 'git'
-depends 'rvm'
-depends 'nginx'
-depends 'nodejs'
+supports "ubuntu", ">= 12.04"
 
-supports 'ubuntu', '>= 12.04'
+depends "application", "2.0.0"
+depends "build-essential"
+depends "git"
+depends "rvm"
+depends "nginx"
+depends "nodejs"
+
